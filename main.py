96301c04c365d9cec8c5728592547f86d3cb898a
sanket <sanketdiwale@gmail.com>
2019-06-11 08:09:42 +0200

    added main script to execute all setup commands

---
+++ main.py
@@ -0,0 +1,40 @@
+import argparse
+import getpass 
+import os
+import json
+
+parser = argparse.ArgumentParser(description='Process some strings')
+parser.add_argument('User',metavar='User',type=str,help='provide username under whose homefolder the project files are stored')
+parser.add_argument('Sitename',metavar='Sitename',type=str,help='provide foldername under which the project files are stored')
+parser.add_argument('SiteURL',metavar='URL',type=str,help='provide URL under which the site is to be served')
+args = parser.parse_args()
+
+# add site to /etc/hosts
+# requires once, execution of chmod +x manage_localhosts.sh
+os.system('addhost '+args.URL)
+
+# setup nginx config
+os.system('sudo python nginx_setup.py '+args.User+' '+args.Sitename+' '+args.URL)
+print('nginx setup complete')
+# setup gunicorn
+with open('sites.json','r') as f:
+    data = json.load(f)
+    ports = list(map(int,data.keys()))
+    port = max(max(ports)+1,8001)
+os.system('sudo python gunicorn_setup.py '+args.User+' '+args.Sitename+' '+str(port))
+print('gunicorn setup complete')
+# setup ssl
+os.system('sudo python ssl_selfsigned_setup.py '+args.Sitename+' '+args.URL)
+
+# reload and restart the services
+# start the gunicorn service as a daemon
+os.system('sudo daemon-reload')
+os.system('sudo systemctl start '+args.Sitename+'.socket')
+os.system('sudo systemctl enable '+args.Sitename+'.socket')
+os.system('sudo daemon-reload')
+
+# enable site and restart nginx
+os.system('sudo nginx_ensite '+args.Sitename)
+os.system('sudo service nginx reload')
+os.system('sudo service nginx restart')
+os.system('sudo service '+args.Sitename+' restart')
